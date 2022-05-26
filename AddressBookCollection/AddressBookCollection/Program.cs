e60d0e376a4059329cf20d532e2ff31fa06d7f05
snehasonwane <snehasonwane01@gmail.com>
2022-05-26 22:07:00 +0530

    [Manasi] Add: Welcome Message

---
+++ AddressBookCollection/AddressBookCollection/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace AddressBookCollection
+{
+  class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Program")
+
+        }
+    }
+}
