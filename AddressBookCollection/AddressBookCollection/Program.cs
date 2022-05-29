281d73e1ad8009805575412ab712cfee2c2be88c
snehasonwane <snehasonwane01@gmail.com>
2022-05-29 12:42:26 +0530

    [Manasi]Add :create Contact

--- AddressBookCollection/AddressBookCollection/Program.cs
+++ AddressBookCollection/AddressBookCollection/Program.cs
@@ -7,8 +7,17 @@ namespace AddressBookCollection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Address Book Program")
-
+            Console.WriteLine("Welcome To Address Book Program");
+            Person person = new Person("Manasi", "Saner", "Nashik", "Maharashtra", "422010", "7020648548", "snehasonwane01@gmail.com");
+            Console.WriteLine(person.getName());
+            Console.WriteLine(person.getlastName());
+            Console.WriteLine(Person.getaddress());
+            Console.WriteLine(person.getcity());
+            Console.WriteLine(person.getstate());
+            Console.WriteLine(person.getzip());
+            Console.WriteLine(person.getphone());
+            Console.WriteLine(person.getemail());
+            Console.Read();
         }
     }
 }
